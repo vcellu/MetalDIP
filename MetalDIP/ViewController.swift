a8745890f5583570d98ed57aca7c7b011f19f8ea
Vittorio Cellucci <vel@qlik.com>
2022-09-24 12:12:45 -0400

    Initial Commit

---
+++ MetalDIP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalDIP
+//
+//  Created by Vittorio Cellucci on 2022-09-24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
