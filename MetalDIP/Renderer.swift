daa3946e1483c320325bb5b7d2c610f529b20fb3
Vittorio Cellucci <vel@qlik.com>
2022-10-01 13:14:19 -0400

    feat: hello triangle

--- MetalDIP/Renderer.swift
+++ MetalDIP/Renderer.swift
@@ -11,21 +11,38 @@ import MetalKit
 class Renderer : NSObject, MTKViewDelegate {
  
   private var metalView: MTKView
+  private var vertextBuffer: MTLBuffer?
   private var commandQueue: MTLCommandQueue?
+  private var pipelineState: MTLRenderPipelineState?
+  private let pipelineDesc = MTLRenderPipelineDescriptor()
   
-  public init(view: MTKView, device: MTLDevice) {
+  init(view: MTKView, device: MTLDevice) {
     metalView = view
     commandQueue = device.makeCommandQueue()
     
+    // Create our vertex data
+    let vertices = [Vertex(color: [1, 0, 0, 1], pos: [-1, -1]),
+                    Vertex(color: [0, 1, 0, 1], pos: [0, 1]),
+                    Vertex(color: [0, 0, 1, 1], pos: [1, -1])]
+    
+    vertextBuffer = device.makeBuffer(bytes: vertices, length: vertices.count * MemoryLayout<Vertex>.stride)
     super.init()
     metalView.delegate = self
+    metalView.enableSetNeedsDisplay = true
     metalView.clearColor = MTLClearColorMake(0.39, 0.58, 0.93, 1.00)
+    
+    guard let library = device.makeDefaultLibrary() else { return }
+    do {
+      pipelineDesc.vertexFunction = library.makeFunction(name: "vertexShader")
+      pipelineDesc.fragmentFunction = library.makeFunction(name: "fragmentShader")
+      pipelineDesc.colorAttachments[0].pixelFormat = view.colorPixelFormat
+      pipelineState = try device.makeRenderPipelineState(descriptor: pipelineDesc)
+    } catch {
+      print("can't create pipeline \(error)")
+    }
   }
-  
-  public func update() {
-  }
-  
-  public func draw(in view: MTKView) {
+    
+  func draw(in view: MTKView) {
     guard
       let commandQueue = commandQueue,
       let renderDescriptor = metalView.currentRenderPassDescriptor,
@@ -35,8 +52,8 @@ class Renderer : NSObject, MTKViewDelegate {
       else {
         return
       }
-    
-    self.update()
+   
+    update(commandEncoder)
     
     commandEncoder.endEncoding()
     commandBuffer.present(drawable)
@@ -44,7 +61,14 @@ class Renderer : NSObject, MTKViewDelegate {
     
   }
   
-  public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+  func update(_ encoder: MTLRenderCommandEncoder) {
+    guard let pipelineState = pipelineState else { return }
+    encoder.setRenderPipelineState(pipelineState)
+    encoder.setVertexBuffer(vertextBuffer, offset: 0, index: 0)
+    encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+  }
+  
+  func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
     
   }
 }
---
+++ MetalDIP/ShaderDefs.h
@@ -0,0 +1,18 @@
+//
+//  ShaderDefs.h
+//  MetalDIP
+//
+//  Created by Vittorio Cellucci on 2022-10-01.
+//
+
+#ifndef ShaderDefs_h
+#define ShaderDefs_h
+
+#include <simd/simd.h>
+
+struct Vertex {
+  vector_float4 color;
+  vector_float2 pos;
+};
+
+#endif /* ShaderDefs_h */
